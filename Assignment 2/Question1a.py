9b2e11598a9592a82e263d46695619ab67ed9498
harsh <harsh.bhattcr@gmail.com>
2020-10-02 19:05:36 -0400

    Question1

--- Assignment 2/Question1a.py
+++ Assignment 2/Question1a.py
@@ -4,14 +4,37 @@ from random import choices
 data_array = []
 m, k = 1, 10
 
+sum = 0
+for i in range(2,k+1):
+    sum += 0.9**i
+
+weights = []
+for i in range(2,k+1):
+    wi = (0.9**i)/sum
+    weights.append(wi)
+print(weights)
+
 
 def generate_data_points(k):
-    array = []
-    array.append(choices([0, 1], [0.5, 0.5])[0])
-    print(array)
+
+    x_values = []
+
+    def generate_y_values():
+        check_sum = 0
+        y = -1
+        for i in range(1,k):
+            check_sum += weights[i-1] * x_values[i]
+        if check_sum >= 0.5:
+            y = x_values[0]
+        else:
+            y = 1 - x_values[0]
+        return y
+
+    x_values.append(choices([0, 1], [0.5, 0.5])[0])
     for i in range(1,k):
-        array.append(choices([array[i-1], (1-array[i-1])], [0.75, 0.25])[0])
-    print(array)
+        x_values.append(choices([x_values[i-1], (1-x_values[i-1])], [0.75, 0.25])[0])
+    y_value = generate_y_values()
+    print(x_values, y_value)
 
 
 for i in range(m):
