a8346f88df857be8b454cc82308115a67cb28c53
harsh <harsh.bhattcr@gmail.com>
2020-10-07 13:00:36 -0400

    Class changes

--- Assignment 2/Question1a.py
+++ Assignment 2/Question1a.py
@@ -1,44 +1,45 @@
 from random import choices
 
-data_array = []
-m, k = 1, 10
-
-# Generate Weights
-weight_sum = 0
-for i in range(2, k + 1):
-    weight_sum += 0.9 ** i
-
-weights = []
-for i in range(2, k + 1):
-    wi = (0.9 ** i) / weight_sum
-    weights.append(wi)
+data_array, weights = [], []
+m, k = 30, 4
 
 
-def generate_y_values(x_values):
-    check_sum = 0
-    y = -1
-    for i in range(1, k):
-        check_sum += weights[i - 1] * x_values[i]
-    if check_sum >= 0.5:
-        y = x_values[0]
-    else:
-        y = 1 - x_values[0]
-    return y
+class GenerateDataset():
+
+    def __init__(self):
+        # Generate Weights
+        weight_sum = 0
+        for i in range(2, k + 1):
+            weight_sum += 0.9 ** i
+
+        for i in range(2, k + 1):
+            wi = (0.9 ** i) / weight_sum
+            weights.append(wi)
+
+    def generate_y_values(self, x_values):
+        check_sum = 0
+        y = -1
+        for i in range(1, k):
+            check_sum += weights[i - 1] * x_values[i]
+        if check_sum >= 0.5:
+            y = x_values[0]
+        else:
+            y = 1 - x_values[0]
+        return y
+
+    def generate_data_points(self, k):
+        x_values = [choices([0, 1], [0.5, 0.5])[0]]
+
+        for i in range(1, k):
+            x_values.append(choices([x_values[i - 1], (1 - x_values[i - 1])], [0.75, 0.25])[0])
+        y_value = self.generate_y_values(x_values)
+        return [x_values, y_value]
+
+    def generate(self, m):
+        for i in range(m):
+            data_array.append(self.generate_data_points(k))
 
 
-def generate_data_points(k):
-    x_values = [choices([0, 1], [0.5, 0.5])[0]]
-
-    for i in range(1, k):
-        x_values.append(choices([x_values[i - 1], (1 - x_values[i - 1])], [0.75, 0.25])[0])
-    y_value = generate_y_values(x_values)
-    return [x_values, y_value]
-
-
-def generate(m):
-    for i in range(m):
-        data_array.append(generate_data_points(k))
-        print(data_array)
-
-
-generate(m)
+obj = GenerateDataset()
+obj.generate(m)
+print(data_array)
--- Assignment 2/Question2a.py
+++ Assignment 2/Question2a.py
@@ -5,29 +5,30 @@ data_array = []
 m, k = 1, 20
 
 
-def generate_y_values(x_values):
-    if x_values[0] == 0:
-        y = Counter(x_values[1:8]).most_common()[0][0]
-    else:
-        y = Counter(x_values[8:15]).most_common()[0][0]
-    return y
+class GenerateDataset():
+
+    def generate_y_values(self, x_values):
+        if x_values[0] == 0:
+            y = Counter(x_values[1:8]).most_common()[0][0]
+        else:
+            y = Counter(x_values[8:15]).most_common()[0][0]
+        return y
+
+    def generate_data_points(self, k):
+        x_values = [choices([0, 1], [0.5, 0.5])[0]]
+
+        for i in range(1, 15):
+            x_values.append(choices([x_values[i - 1], (1 - x_values[i - 1])], [0.75, 0.25])[0])
+        for i in range(15, k + 1):
+            x_values.append(choices([1, 0], [0.5, 0.5])[0])
+        y_value = self.generate_y_values(x_values)
+        return [x_values, y_value]
+
+    def generate(self, m):
+        for i in range(m):
+            data_array.append(self.generate_data_points(k))
 
 
-def generate_data_points(k):
-    x_values = [choices([0, 1], [0.5, 0.5])[0]]
-
-    for i in range(1, 15):
-        x_values.append(choices([x_values[i - 1], (1 - x_values[i - 1])], [0.75, 0.25])[0])
-    for i in range(15, k+1):
-        x_values.append(choices([1, 0], [0.5, 0.5])[0])
-    y_value = generate_y_values(x_values)
-    return [x_values, y_value]
-
-
-def generate(m):
-    for i in range(m):
-        data_array.append(generate_data_points(k))
-
-
-generate(m)
+obj = GenerateDataset()
+obj.generate(m)
 print(data_array)
